6ca78c1244198954171aa1b688f5bb9b2f027b9c
Derek Sivers <derek@sivers.org>
2013-07-16 13:54:05 +1200

    blog templates

--- make-blog.rb
+++ make-blog.rb
@@ -1,20 +1,30 @@
 require 'erb'
-header_template = ERB.new(File.read('templates/header.erb'))
-footer_template = ERB.new(File.read('templates/footer.erb'))
+
+def template(name)
+  ERB.new(File.read("templates/#{name}.erb"))
+end
 
 Dir['blog/*'].each do |d|
   /(\d{4}-\d{2}-\d{2})-(\S+)/.match File.basename(d)
-  date = $1
-  url = $2
+  @date = $1
+  @year = @date[0,4]
+  @url = $2
   lines = File.readlines(d)
   /<!--\s+(.+)\s+-->/.match lines.shift
-  title = $1
+  @title = $1
   body = lines.join('')
 
-  @pagetitle = title
+  @pagetitle = "#{@title} | Derek Sivers"
   @bodyid = 'oneblog'
-  html = header_template.result + body + footer_template.result
 
-  File.open("site/#{url}", 'w') {|f| f.puts html }
+  # PAGE STRUCTURE
+  html = template('header').result
+  html << template('bloghead').result
+  html << body
+  html << template('blogfoot').result
+  html << template('comments').result
+  html << template('footer').result
+
+  File.open("site/#{@url}", 'w') {|f| f.puts html }
 end
 
