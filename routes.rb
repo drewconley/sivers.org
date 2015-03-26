641c9043ab1273d4ff5077efdb4b6da24954bb9f
Derek Sivers <derek@sivers.org>
2015-03-26 13:56:11 +1300

    thanks and sorry are now static + JavaScript

--- routes.rb
+++ routes.rb
@@ -2,10 +2,12 @@ require 'sinatra/base'
 require 'json'
 require_relative 'models.rb'
 
-## DYNAMIC PATHS for nginx to pass to proxy:
+## DYNAMIC (non-static) parts of sivers.org:
+# 1. posting a comment
+# 2. 
+
+## URL paths for nginx to pass to proxy:
 # /comments
-# /thanks/
-# /sorry/
 # /list/
 # /u/
 # /ayw/
@@ -26,22 +28,6 @@ class SiversOrg < Sinatra::Base
 		else		# bad
 			redirect request.referrer
 		end
-	end
-
-	# THANKS - for what?
-	get %r{\A/thanks/([a-z]+)\Z} do |forwhat|
-		@forwhat = forwhat
-		@bodyid = 'thanks'
-		@pagetitle = 'Thank you!'
-		erb :thanks
-	end
-
-	# SORRY - for what?
-	get %r{\A/sorry/([a-z]+)\Z} do |forwhat|
-		@forwhat = forwhat
-		@bodyid = 'sorry'
-		@pagetitle = 'Sorry!'
-		erb :sorry
 	end
 
 	# LIST: pre-authorized URL to show form for changing settings / unsubscribing
@@ -65,13 +51,13 @@ class SiversOrg < Sinatra::Base
 	# LIST: handle posting of list signup or changing settings
 	post '/list' do
 		EmailList.update(request.env)
-		redirect '/thanks/list'
+		redirect '/thanks?for=list'
 	end
 
 	# DOWNLOAD: lopass auth to get a file from S3
 	get %r{\A/download/([0-9]+)/([a-zA-Z0-9]{4})/([a-zA-Z0-9\._-]+)\Z} do |person_id, lopass, filename|
 		p = Person.where(id: person_id, lopass: lopass).first
-		redirect '/sorry/login' unless p
+		redirect '/sorry?for=login' unless p
 		nu = {person_id: p.id, statkey: 'download', statvalue: filename}
 		Userstat.create(nu)
 		redirect AYW.url_for(filename)
@@ -87,13 +73,13 @@ class SiversOrg < Sinatra::Base
 		f = Formletter[Sivers.config['formletter_download_pdf']]
 		h = {profile: 'derek@sivers', subject: p.firstname + ' - How to Call Attention To Your Music'}
 		f.send_to(p, h)
-		redirect '/thanks/pdf'
+		redirect '/thanks?for=pdf'
 	end
 
 	# PASSWORD: semi-authorized. show form to make/change real password
 	get %r{\A/u/([0-9]+)/([a-zA-Z0-9]{8})\Z} do |person_id, newpass|
 		p = Person.where(id: person_id, newpass: newpass).first
-		redirect '/sorry/badurlid' unless p
+		redirect '/sorry?for=badurlid' unless p
 		@person_id = person_id
 		@newpass = newpass
 		@bodyid = 'newpass'
@@ -104,8 +90,8 @@ class SiversOrg < Sinatra::Base
 	# PASSWORD: posted here to make/change it. then log in with cookie
 	post '/u/password' do
 		p = Person.where(id: params[:person_id], newpass: params[:newpass]).first
-		redirect '/sorry/badurlid' unless p
-		redirect '/sorry/shortpass' unless params[:password].to_s.size >= 4
+		redirect '/sorry?for=badurlid' unless p
+		redirect '/sorry?for=shortpass' unless params[:password].to_s.size >= 4
 		p.set_password(params[:password])
 		p.set_newpass
 		if ['sivers.org', 'sivers.dev', 'example.org'].include? request.env['SERVER_NAME']
@@ -125,23 +111,23 @@ class SiversOrg < Sinatra::Base
 	# PASSWORD: email posted here. send password reset link
 	post '/u/forgot' do
 		p = Person[email: params[:email]]
-		redirect '/sorry/noemail' unless p
+		redirect '/sorry?for=noemail' unless p
 		p.set_newpass
 		f = Formletter[Sivers.config['formletter_password_reset']]
 		h = {profile: 'derek@sivers', subject: p.firstname + ' - your password reset link'}
 		f.send_to(p, h)
-		redirect '/thanks/reset'
+		redirect '/thanks?for=reset'
 	end
 
 	# AYW post code word + name & email. if right, emails login link
 	# (if you are reading this, yes the codeword is here. it's intentionally not very secret.)
 	post '/ayw/proof' do
-		redirect '/sorry/aywcode' unless /utopia/i === params[:code]
+		redirect '/sorry?for=aywcode' unless /utopia/i === params[:code]
 		p = AYW.update(request.env)
 		f = Formletter[Sivers.config['formletter_ayw_bought']]
 		h = {profile: 'derek@sivers', subject: p.firstname + ' - your MP3 download link'}
 		f.send_to(p, h)
-		redirect '/thanks/ayw'
+		redirect '/thanks?for=ayw'
 	end
 
 	# log in form to get to AYW MP3 download area
@@ -161,7 +147,7 @@ class SiversOrg < Sinatra::Base
 			response.set_cookie('ok', value: ok, path: '/', httponly: true)
 			redirect '/ayw/list'
 		else
-			redirect '/sorry/badlogin'
+			redirect '/sorry?for=badlogin'
 		end
 	end
 
@@ -177,7 +163,7 @@ class SiversOrg < Sinatra::Base
 	# AYW MP3 downloads 
 	get %r{\A/ayw/download/([A-Za-z-]+.zip)\Z} do |zipfile|
 		p = Login.get_person_from_cookie(request.cookies['ok'])
-		redirect '/sorry/login' unless p
+		redirect '/sorry?for=login' unless p
 		redirect '/ayw/list' unless %w(AnythingYouWant.zip CLASSICAL-AnythingYouWant.zip COUNTRY-AnythingYouWant.zip FOLK-AnythingYouWant.zip JAZZ-AnythingYouWant.zip OTHER-AnythingYouWant.zip POP-AnythingYouWant.zip ROCK-AnythingYouWant.zip SAMPLER-AnythingYouWant.zip SINGSONG-AnythingYouWant.zip URBAN-AnythingYouWant.zip WORLD-AnythingYouWant.zip).include? zipfile
 		send_file "/srv/http/downloads/#{zipfile}"
 	end
